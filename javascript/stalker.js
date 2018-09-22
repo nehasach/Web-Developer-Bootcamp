abcde196861ae4229aab1ff567124511acd52b78
Neha Sachdev <nehasach@usc.edu>
2018-09-21 19:24:14 -0500

    Javascript & Bootstrap

---
+++ javascript/stalker.js
@@ -0,0 +1,6 @@
+alert("Neha");
+var fn=prompt("Enter your fn");
+var ln=prompt("enter your lastname");
+var age=prompt("enter your age");
+console.log("Your name is "+fn+" "+ln);
+console.log("you are "+age+" years old");
